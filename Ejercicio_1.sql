f05b49e8a5af00db040f2dce80f4d541f4a6429c
MiguelSandovalMillan <2408819j@umich.mx>
2026-09-23 12:49:15 -0600

    22/09

---
+++ Ejercicio_1.sql
@@ -0,0 +1,19 @@
+-- Crear la tabla items_2
+CREATE TABLE items_2 (
+    id INT PRIMARY KEY,
+    price INT
+);
+
+-- Inserción de los datos
+INSERT INTO items_2 (id, price) VALUES
+(4, 19),
+(5, 16),
+(6, 13),
+(7, 21),
+(8, 16),
+(9, 23),
+(10, 27),
+(11, 18),
+(12, 21),
+(13, 22);
+
